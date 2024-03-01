17f5d288806a434d310f30e09c1b27edca8b01da
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-03-01 11:52:15 +0530

    created abouttravel component

--- src/components/AboutTravel.jsx
+++ src/components/AboutTravel.jsx
@@ -1,12 +1,39 @@
 import React from "react";
-import './AboutTravel.css';
+import "./AboutTravel.css";
 
-function AboutTravel(){
-    return(
-        <div className="about-travel">
-
+function AboutTravel() {
+  return (
+    <div className="about-travel">
+      <h1>About Travel</h1>
+      <p className="about-travel-details">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod
+        tempor incididunt u lobore et dolore
+        <br /> mangna aliqua. Ut enim ad minim veniam, quis nostrud exercitaton
+        uliamco laboris nisi ut aliquip ex ea com-
+        <br /> modo consequat.
+      </p>
+      <div className="d-flex flex-row justify-content-center">
+        <div className="p-4">
+            <h5>Lorem Ipsum Dolor</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisc-<br/>ing elit, sed do eius mod tempor</p>
         </div>
-    );
+        <div className="p-4">
+        <h5>Lorem Ipsum Dolor</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisc-<br/>ing elit, sed do eius mod tempor</p>
+        </div>
+      </div>
+      <div className="d-flex flex-row justify-content-center ">
+        <div className="p-4">
+        <h5>Lorem Ipsum Dolor</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisc-<br/>ing elit, sed do eius mod tempor</p>
+        </div>
+        <div className="p-4">
+        <h5>Lorem Ipsum Dolor</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisc-<br/>ing elit, sed do eius mod tempor</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutTravel;
